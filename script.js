a24735a4710333bb3aad77f6bfe3f42390926425
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-07-03 14:13:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-const video = document.getElementById('video');
-const playerButton = document.querySelector('.player__button');
+const video = document.querySelector('.viewer');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('[name="volume"]');
+const playbackSpeed = document.querySelector('[name="playbackSpeed"]');
+const rewind = document.querySelector('.rewind');
+const forward = document.querySelector('.forward');
 const progress = document.querySelector('.progress');
-const progressBarFilled = document.querySelector('.progress__filled');
-const volumeInput = document.querySelector('.volume');
-const playbackSpeedInput = document.querySelector('.playbackSpeed');
-const rewindButton = document.querySelector('.rewind');
-const forwardButton = document.querySelector('.forward');
+const progressBar = document.querySelector('.progress__filled');
 
-let isPlaying = false;
-
-playerButton.addEventListener('click', togglePlayPause);
-progress.addEventListener('input', updateProgressBar);
-volumeInput.addEventListener('input', updateVolume);
-playbackSpeedInput.addEventListener('input', updatePlaybackSpeed);
-rewindButton.addEventListener('click', rewindVideo);
-forwardButton.addEventListener('click', forwardVideo);
-
-function togglePlayPause() {
-  if (isPlaying) {
-    video.pause();
-    playerButton.dataset.icon = '►';
-    isPlaying = false;
-  } else {
+function togglePlay() {
+  if (video.paused) {
     video.play();
-    playerButton.dataset.icon = '❚ ❚';
-    isPlaying = true;
+  } else {
+    video.pause();
   }
 }
 
-function updateProgressBar() {
-  const progressPercentage = (video.currentTime / video.duration) * 100;
-  progressBarFilled.style.flexBasis = `${progressPercentage}%`;
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
 }
 
-function updateVolume() {
-  video.volume = volumeInput.value / 100;
+function handleRangeUpdate() {
+  video[this.name] = this.value;
 }
 
-function updatePlaybackSpeed() {
-  video.playbackRate = playbackSpeedInput.value;
+function skip(seconds) {
+  video.currentTime += seconds;
 }
 
-function rewindVideo() {
-  video.currentTime -= 10;
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
-function forwardVideo() {
-  video.currentTime += 25;
-}
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+volume.addEventListener('change', handleRangeUpdate);
+volume.addEventListener('mousemove', handleRangeUpdate);
+playbackSpeed.addEventListener('change', handleRangeUpdate);
+playbackSpeed.addEventListener('mousemove', handleRangeUpdate);
+rewind.addEventListener('click', () => skip(-10));
+forward.addEventListener('click', () => skip(25));
+
+progress.addEventListener('click', (e) => {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+});
